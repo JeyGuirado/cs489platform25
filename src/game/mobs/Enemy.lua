797736936534d69a12ca8eace78531312e91b85d
somerscyn <144164003+somerscyn@users.noreply.github.com>
2025-04-11 18:37:50 -0400

    tweening text + particle explosion

--- src/game/mobs/Enemy.lua
+++ src/game/mobs/Enemy.lua
@@ -1,5 +1,6 @@
 local Class = require "libs.hump.class"
 local Hbox = require "src.game.Hbox"
+local Timer = require "libs.hump.timer"
 
 local Enemy = Class{}
 function Enemy:init()
@@ -19,6 +20,13 @@ function Enemy:init()
     self.damage = 10 -- mob's damage
     self.died = false
     self.score = 100 -- score to kill this mob 
+    self.damageNumbers = {} 
+    self.particles = love.graphics.newParticleSystem(love.graphics.newImage("graphics/particles/20.png"), 100)
+    self.particles:setParticleLifetime(0.5, 1) 
+    self.particles:setEmissionRate(0)
+    self.particles:setSpeed(20, 50)
+    self.particles:setSizes(0.5, 1)
+    self.particles:setLinearAcceleration(-10, -10, 10, 10)
 end
 
 function Enemy:getDimensions() -- returns current Width,Height
@@ -75,6 +83,37 @@ end
 
 function Enemy:hit(damage)
     self.hp = self.hp - damage
+
+    --display text
+    self:showDamage(damage)
+
+    if self.hp <= 0 then
+        self.died = true
+        self.state = "die"
+        self.particles:setPosition(self.x + self:getDimensions()/2,
+            self.y + self:getDimensions()/2)
+        self.particles:setEmissionRate(100)
+        self.particles:setColors(1, 1, 0, 1, 1, 0, 0, 0)
+        self.particles:emit(100)
+        --self.particles:stop()
+    end
+    --trigger particles
+end
+
+function Enemy:showDamage(damage)
+    local num = DamageNumber(damage)
+    num:setCoord(self.x + self:getDimensions()/2,
+        self.y + self:getDimensions()/2)
+    table.insert(self.damageNumbers,num)
+
+    Timer.tween(0.8, dmg, { y = dmg.y - 20, alpha = 0 }, 'linear', function()
+        for i, v in ipairs(self.damageNumbers) do
+            if v == dmg then
+                table.remove(self.damageNumbers, i)
+                break
+            end
+        end
+    end)
 end
 
 function Enemy:getHbox(boxtype)
